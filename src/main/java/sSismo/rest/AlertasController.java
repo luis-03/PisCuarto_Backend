a19fff04c47f941f4d9c9630ce4296d1a1248c93
luis <luis-03>
2024-02-18 02:30:30 -0500

    Mostar l alerta por 30s

--- src/main/java/sSismo/rest/AlertasController.java
+++ src/main/java/sSismo/rest/AlertasController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import sSismo.servicios.SismoService;
 import sSismo.utilidades.GeneradorAlertas;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 @RestController
 @RequestMapping(value = "/api/v1")
 @CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.POST,
@@ -21,12 +24,37 @@ public class AlertasController {
     @Autowired
     private GeneradorAlertas generadorAlertas;
 
+    private double intensidadActual;
+    private Timer timer;
+
     @GetMapping("/alertas")
     public AlertaResponse obtenerAlertaSismo() {
-        double intensidadSismo = sismoService.obtenerIntensidadSismo();
+        double nuevaIntensidad = sismoService.obtenerIntensidadSismo();
         String fecha = sismoService.obtenerFecha();
         String hora = sismoService.obtenerHora();
-        String alerta = generadorAlertas.generarAlerta(intensidadSismo);
-        return new AlertaResponse(intensidadSismo, fecha, hora, alerta);
+        
+        if (nuevaIntensidad > intensidadActual) {
+            intensidadActual = nuevaIntensidad;
+            // Reiniciar el temporizador para 30 segundos
+            reiniciarTimer();
+        }
+
+        String alerta = generadorAlertas.generarAlerta(intensidadActual);
+        return new AlertaResponse(intensidadActual, fecha, hora, alerta);
+    }
+
+    // Método para reiniciar el temporizador
+    private void reiniciarTimer() {
+        if (timer != null) {
+            timer.cancel();
+        }
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // Al cumplirse el tiempo, la intensidad actual se reinicia a cero
+                intensidadActual = 0;
+            }
+        }, 30000); // 30 segundos en milisegundos
     }
 }
